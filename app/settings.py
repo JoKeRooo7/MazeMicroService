e42f0200029fbae17b1529f3dae87f60bbf77d56
JoKeRooo7 <renal98@mail.ru>
2025-01-10 15:18:56 +0300

    update

--- app/settings.py
+++ app/settings.py
@@ -4,20 +4,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    APP_HOST: str = Field("0.0.0.0", env="HOST")
-    APP_PORT: int = Field(8888, env="PORT")
-    GIF_DIRECTORY: str = Field("files", env="MAZE_GIF_DIRECTORY")
-    GIF_FILE_NAME: str = Field("maze.gif", env="MAZE_GIF_FILE_NAME")
-    MAZE_ROWS: int = Field(35, env="MAZE_STD_ROWS")
-    MAZE_COLS: int = Field(35, env="MAZE_STD_COLS")
-    IMAGE_LENGHT: int = Field(8, env="MAZE_IMAGE_LENGHT")
-    IMAGE_HEIGHT: int = Field(8, env="MAZE_IMAGE_HEIGHT")
-    WALL_COLOR: str = Field("#000000", env="MAZE_WALL_COLOR")
-    POINT_COLOR: str = Field("#28a745", env="MAZE_POINT_COLOR")
-    WAY_COLOR: str = Field("#28a745", env="MAZE_WAY_COLOR")
-    POINT_SIZE: int = Field(25, env="MAZE_POINT_SIZE")
-    WALL_THICKNESS: int = Field(3, env="MAZE_WALL_THICKNESS")
-    GIF_DELAY: float = Field(0.3, env="MAZE_WALL_THICKNESS")
+    HOST: str = "0.0.0.0"
+    PORT: int = 8888
+    GIF_DIRECTORY: str = "files"
+    GIF_FILE_NAME: str = "maze.gif"
+    MAZE_ROWS: int = 35
+    MAZE_COLS: int = 35
+    IMAGE_LENGHT: int = 8
+    IMAGE_HEIGHT: int = 8
+    WALL_COLOR: str = "#000000"
+    POINT_COLOR: str = "#28a745"
+    WAY_COLOR: str = "#28a745"
+    POINT_SIZE: int = 25
+    WALL_THICKNESS: int = 3
+    GIF_DELAY: float = 0.3
 
 
     def __init__(self, **kwargs):
